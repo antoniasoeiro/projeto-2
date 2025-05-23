446031a171326cf77295899e4ba0ae2571f1a352
antoniasoeiro <mariaantonia.delimasoeiro.ismart@gmail.com>
2025-05-23 11:00:58 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,17 +4,27 @@ import numpy as np
 
 st.write("""
 # Pesquisa de preferências
-Olá aluno(a)! Responda sabiamente as perguntas abaixo:
+Olá aluno(a) da FGV Comunicação! Responda *sabiamente* as perguntas abaixo:
 """)
 
 st.text_input("Digite seu nome:")
 st.text_input("Digite sua idade:")
 st.text_input("Digite o seu período:")
 
-option = st.selectbox(
+prof = st.selectbox(
     "Qual o seu professor favorito do 1 período da ECMI?",
     ("Eurico", "Josir", "Matheus", "Renata", "Victor"),
 )
 
-if option == "Josir":
+if prof == "Josir":
     st.write("Ótima escolha!")
+
+disciplina = st.selectbox(
+    "Qual a sua disciplina preferida do 1 período?",
+    ("Ciências de Dados", "Ciências Sociais", "Comunicação e Cultura Digital", "Programação", "Teorias da Comunicação")
+)
+
+if disciplina == "Programação":
+    st.write("Ótima escolha!")
+
+st.write("Obrigada!")
