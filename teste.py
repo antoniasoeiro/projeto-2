8e4cf2fd2589eec17aa65b5718ff6108e7c64558
antoniasoeiro <mariaantonia.delimasoeiro.ismart@gmail.com>
2025-05-23 10:54:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,9 +3,17 @@ import pandas as pd
 import numpy as np
 
 st.write("""
-# App da Antonia
-oieeeeee
+# Pesquisa de preferências
+Olá aluno(a)! Responda sabiamente as perguntas abaixo:
 """)
 
 st.text_input("Digite seu nome:")
-st.write("Que nome legal!")
+st.text_input("Digite sua idade:")
+st.text_input("Digite o seu período:")
+
+option = st.selectbox(
+    "Qual o seu professor favorito do 1 período da ECMI?",
+    ("Eurico", "Josir", "Matheus", "Renata", "Victor"),
+)
+
+st.write("Você escolheu:", option)
